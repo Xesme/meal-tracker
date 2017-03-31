dcb9b2006b2df7d565349d167a02a51211b83b63
xa <xiaesmeamen@me.com>
2017-03-31 10:07:35 -0700

    set up meal model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Meals!';
+  meals: Meal[] = [
+    new Meal("Hamburger", "Tried a boca patty!", 452),
+    new Meal("Hamburger", "Didn't get a soda or cheese on my burger!", 354),
+    new Meal("Fries", "I only ate half of them.", 365)
+  ];
+
+}
+export class Meal {
+  constructor(public name: string, public details: string, public calories: number) { }
 }
